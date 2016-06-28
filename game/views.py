02160adac4e1ac500bda7bbd8c1ce5be5c79552e
chriswhsu <chriswhsu@berkeley.edu>
2016-06-28 12:59:52 -0700

    adding bootstrap

--- game/views.py
+++ game/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 from django.http import HttpResponse
 
-
+# Game page
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "base.html")
