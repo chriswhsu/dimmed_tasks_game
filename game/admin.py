78c07c56255d06b22722bea84d85c8511875b63b
chriswhsu <chriswhsu@berkeley.edu>
2016-06-29 15:47:50 -0700

    start building up class/data model and admin pages.

--- game/admin.py
+++ game/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+
 from game.models import *
+
 
 # Register your models here.
 
 class TaskTypeAdmin(admin.ModelAdmin):
-    list_display = ( 'id','name',)
+    list_display = ('id', 'name',)
     list_editable = ('name',)
 
 
 admin.site.register(TaskType, TaskTypeAdmin)
+
+
+class GameRoundUserInLine(admin.TabularInline):
+    model = GameRoundUser
+    list_display = ('user', 'name')
+
+
+class GameRoundAdmin(admin.ModelAdmin):
+    list_display = ('datetime',)
+    inlines = [GameRoundUserInLine]
+
+
+admin.site.register(GameRound, GameRoundAdmin)
