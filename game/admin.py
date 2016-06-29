d6bb6ebfeec5a40dbaf04048a7d0a6ecbbd2f331
chriswhsu <chriswhsu@berkeley.edu>
2016-06-29 15:25:14 -0700

    start building up class/data model and admin pages.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from game.models import *
 
 # Register your models here.
+
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ( 'id','name',)
+    list_editable = ('name',)
+
+
+admin.site.register(TaskType, TaskTypeAdmin)
--- game/models.py
+++ game/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class GameRound(models.Model):
+    datetime = models.DateTimeField()
+    users = models.ManyToManyField(User, through="GameRoundUser")
+    tasktype = models.ManyToManyField(TaskType, through="GameRoundTaskType")
+
+
+class GameRoundUser(models.Model):
+    gameround = models.ForeignKey(GameRound)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=30)
+    occupation = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    province = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1)
+
+
+class GameRoundTaskType(models.Model):
+    gameround = models.ForeignKey(GameRound)
+    tasktype = models.ForeignKey(TaskType)
+    relative_weight = models.IntegerField()
