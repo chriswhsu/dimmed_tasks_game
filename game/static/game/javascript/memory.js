baaa2fe684b08a1fe181e19e2bc7326fe9a7c4ae
chriswhsu <chriswhsu@berkeley.edu>
2016-08-31 15:13:45 -0700

    start working on 2048 game type.

--- game/static/game/javascript/memory.js
+++ game/static/game/javascript/memory.js
@@ -125,19 +125,6 @@ $(document).ready(function () {
     $('#resetButton').click(ResetGame);
 
 
-    var dl = 1.0 - $("#dim_level").text();
-    $('#screen').css({opacity: dl, 'width': $(document).width(), 'height': $(document).height()});
-
-    $(function () {
-        var pop = function () {
-            var dimmlevel = 1 - $("input[name=optradio]:checked").val();
-            $('#screen').css({opacity: dimmlevel, 'width': $(document).width(), 'height': $(document).height()});
-        };
-
-        $('#dim_button').click(pop);
-    });
-
-
     function next_iteration_ajax(clicks) {
         var data = {
             'clicks': clicks,
