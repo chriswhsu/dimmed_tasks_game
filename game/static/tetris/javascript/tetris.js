653748d859a33d3460158b041e29af14d0bc3e8b
chriswhsu <chriswhsu@berkeley.edu>
2016-09-01 14:13:00 -0700

    adding sample image oriented correctly.  Still need to dim it.

--- game/static/tetris/javascript/tetris.js
+++ game/static/tetris/javascript/tetris.js
@@ -41,7 +41,6 @@ if (!window.requestAnimationFrame) { // http://paulirish.com/2011/requestanimati
 
 var KEY = {ESC: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40},
     DIR = {UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3, MIN: 0, MAX: 3},
-    stats = new Stats(),
     canvas = get('canvas'),
     ctx = canvas.getContext('2d'),
     ucanvas = get('upcoming'),
@@ -143,7 +142,6 @@ function randomPiece() {
 
 function run() {
 
-    showStats(); // initialize FPS counter
     addEvents(); // attach keydown and resize events
 
     var last = now = timestamp();
@@ -152,7 +150,6 @@ function run() {
         now = timestamp();
         update(Math.min(1, (now - last) / 1000.0)); // using requestAnimationFrame have to be able to handle large delta's caused when it 'hibernates' in a background or non-visible tab
         draw();
-        stats.update();
         last = now;
         requestAnimationFrame(frame, canvas);
     }
@@ -161,11 +158,6 @@ function run() {
     reset();  // reset the per-game variables
     frame();  // start the first frame
 
-}
-
-function showStats() {
-    stats.domElement.id = 'stats';
-    get('menu').appendChild(stats.domElement);
 }
 
 function addEvents() {
